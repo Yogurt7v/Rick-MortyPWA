b23ea21a208046b1c6af16bc68614497543ee7e9
Yogurt7v <egorov-2k@yandex.ru>
2024-07-17 00:05:54 +0300

    fix: никак не получается подкрепить иконки.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register("./sw.js")
     .then((reg) => console.log("Service Worker Registered", reg))
     .catch((err) => console.error("Service Worker registration failed", err));
 } else {
