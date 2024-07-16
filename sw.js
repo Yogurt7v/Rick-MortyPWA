5df6b2a70fc6437157bf33538840442e48a637f3
Yogurt7v <egorov-2k@yandex.ru>
2024-07-16 20:02:41 +0300

    fix: подправлен динамический кэш

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const staticCacheName = "static-site-V1";
-const dynamicCacheName = "dynamic-site-V1";
+const dynamicCacheName = "dynamic-site-V2";
 
 const ASSETS = [
   "/",
@@ -9,10 +9,11 @@ const ASSETS = [
   "/app.js",
   "/src/App.jsx",
   "/src/main.jsx",
+  "./error.html",
   "/src/pages/Categories.jsx",
   "/src/assets/Rick_and_Morty.svg.png",
   "/src/assets/background.jpg",
-]; 
+];
 
 self.addEventListener("install", async () => {
   const cache = await caches.open(staticCacheName);
@@ -31,26 +32,17 @@ self.addEventListener("activate", async () => {
 });
 
 self.addEventListener("fetch", (event) => {
-    // event.respondWith(
-    //   caches.match(event.request).then((response) => {
-    //     return response || fetch(event.request).then((response) => {
-    //       return caches.open(dynamicCacheName).then((cache) => {
-    //         cache.put(event.request.url, response.clone())
-    //         return response;
-    //       });
-    //     });
-    //   })
-    // )
-
   event.respondWith(cacheFirst(event.request));
 });
 
 async function cacheFirst(request) {
   const cached = await caches.match(request);
   try {
-    return cached ?? await fetch(request).then((response) => {
-      console.log("response", response)
-    return networkFirst(response)});
+    return cached ?? await fetch(request)
+    .then((response) => {
+      console.log("### Caching", response);
+        return networkFirst(request);
+      })
   } catch (e) {
     return networkFirst(request);
   }
@@ -58,12 +50,13 @@ async function cacheFirst(request) {
 
 async function networkFirst(request) {
   const cache = await caches.open(dynamicCacheName);
+
   try {
     const response = await fetch(request);
     await cache.put(request, response.clone());
     return response;
   } catch (e) {
     const cached = await cache.match(request);
-    return cached ?? (await caches.match("/index.html"));
+    return cached ?? await caches.match("./error.html");
   }
 }
