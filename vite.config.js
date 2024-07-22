b6bcfeabf7bc8f66f0365b3a76d7584d8c00d351
Yogurt7v <egorov-2k@yandex.ru>
2024-07-22 14:42:49 +0300

    fix: добавлена рабочая PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 // import manifest from "./manifest.json";
 
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+
+    VitePWA({
+      includeAssets:['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Rick and Morty Routing, PWA',
+        short_name: 'Rick and Morty Routing, PWA',
+        description: 'Rick and Morty Routing, PWA',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'icons96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'icons192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './public/icons512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: "maskable"
+          },
+          
+        ]
+    }})
+  ],
 
 });
 
